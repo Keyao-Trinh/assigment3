009ef2a42a027d13b78b9854349b525c4e3d6856
Keyao-Trinh <trinhkeyao@gmail.com>
2026-03-16 10:34:22 -0400

    Update CubicInput.tsx

--- cube/src/Components/CubicInput.tsx
+++ cube/src/Components/CubicInput.tsx
@@ -3,8 +3,41 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+
+export const Input = () => {
+  console.log("inputed the input                     .");
+
+  return (
+    <div>
+      <h1>
+        Cubic Solver
+      </h1>
+
+      <form name="theform" id="form" onSubmit={handleSubmit}>
+
+        <label for="a">a-vaule:</label> 
+        <input type="number" vaule={a} name="a" required onChange={(e) => setA(Number(e.target.vaule))}></input>
+        <br></br>
+        <label for="b">b-vaule:</label>
+        <input type="number" id="b" name="b" required onChange={(e) => setB(Number(e.target.vaule))}></input>
+        <br></br>
+        <label for="c">c-vaule:</label>
+        <input type="number" id="c" name="c" required onChange={(e) => setC(Number(e.target.vaule))}></input>
+        <br></br>
+        <label for="d">d-vaule:</label> 
+        <input type="number" id="d" name="d" required onChange={(e) => setD(Number(e.target.vaule))}></input>
+        <br></br>
+        <input type="submit" vaule="Calculate" id="submit">SAVE</input>
+
+      </form>
+
+    </div>
+  );
+};
+
+
+// createRoot(document.getElementById('root')!).render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+// )
