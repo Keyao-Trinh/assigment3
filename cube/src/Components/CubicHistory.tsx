f410be80288beb475bbeac6fe08a6a17d93776dd
Keyao-Trinh <trinhkeyao@gmail.com>
2026-03-22 19:39:19 -0400

    Update CubicHistory.tsx

--- cube/src/Components/CubicHistory.tsx
+++ cube/src/Components/CubicHistory.tsx
@@ -1,6 +1,15 @@
 export const history = () => {
+    // set a,b,c,d to old vaules
+    //   
+    //
+    //  const [a, setA] = useState<number>(0);
+  // const [b, setB] = useState<number>(0);
+  // const [c, setC] = useState<number>(0);
+  // const [d, setD] = useState<number>(0);
+    // onclick
 return(
     <div>
+        // <input type="submit" vaule="Calculate" id="submit">SAVE</input>
 <table>
 <tr>
 <td>'a'</td>
