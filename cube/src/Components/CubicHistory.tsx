3dde7bbe6fa9d0eacb356f8e8d65cb5de6158705
342886108 <isabella.trinh@student.tdsb.on.ca>
2026-03-31 10:18:21 -0400

    useEffect and trying props

--- cube/src/Components/CubicHistory.tsx
+++ cube/src/Components/CubicHistory.tsx
@@ -1,9 +1,9 @@
-import {useRef} from "react";
-import { Input } from "./cube/src/Components/CubicInput.tsx";
+import {useRef, useState, useEffect} from "react";
+import { Input } from "./CubicInput.tsx";
 
 
 
-export const history = () => {
+export const History = (aVaule:number, bVaule:number, cVaule:number, dVaule:number, count:number) => {
     // set a,b,c,d to old vaules
     //   
     //
@@ -11,11 +11,22 @@ export const history = () => {
   // const [b, setB] = useState<number>(0);
   // const [c, setC] = useState<number>(0);
   // const [d, setD] = useState<number>(0);
+useEffect (() => {
 
-  const aRef=useRef<number>(Input.a);
-  const bRef=useRef<number>(Input.b);
-  const cRef=useRef<number>(Input.c);
-  const dRef=useRef<number>(Input.d);
+  const aRef=useRef<number>(a);
+  const bRef=useRef<number>(b);
+  const cRef=useRef<number>(c);
+  const dRef=useRef<number>(d);
+
+}, [count]);
+  
+  let [a, setA] = useState<number>(0);
+  let [b, setB] = useState<number>(0);
+  let [c, setC] = useState<number>(0);
+  let [d, setD] = useState<number>(0);
+  
+  
+
 
   
 
@@ -23,7 +34,7 @@ return(
     <div>
 
 <table
-   onClick={() => setA(aRef) }
+   onClick={() => (Input.setA(Number(aRef)), Input.setB(Number(bRef)), Input.setC(Number(cRef)), Input.setD(Number(dRef)))}
   // document.getElementById("a") as HTMLElement 
   >
 <tr>
--- cube/src/Components/CubicInput.tsx
+++ cube/src/Components/CubicInput.tsx
@@ -1,23 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { History } from "./cube/src/Components/CubisHistory.tsx";
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import { History } from "./cube/src/Components/CubisHistory.tsx";
 
 import React, { useRef, useState } from 'react';
 
-import './index.css'
-import App from './App.tsx'
+// import './index.css'
 //uneState saves adnd rerenders. ref just saves 
+
+let [a, setA] = useState<number>(0);
+let [b, setB] = useState<number>(0);
+let [c, setC] = useState<number>(0);
+let [d, setD] = useState<number>(0);
+
+
 export const Input = () => {
   console.log("inputed the input                     .");
-  let [a, setA] = useState<number>(0);
-  let [b, setB] = useState<number>(0);
-  let [c, setC] = useState<number>(0);
-  let [d, setD] = useState<number>(0);
-  const handleSubmit (event: React.FormEvent) => {
+ 
+  const handleSubmit = (event: React.FormEvent) => {
     event.preventDefault();
     //const formData = new FormData(form);  
   };
-
 
 
   return (
@@ -28,46 +30,34 @@ export const Input = () => {
 
       <form name="theform" id="form" onSubmit={handleSubmit}>
 
-        <label for="a">a-vaule:</label> 
-        <input type="number" /*vaule={a}*/ id="a" name="a" required onChange={(e) => setA(Number(e.target.value))}></input>
+        <label /*for="a"*/>a-vaule:</label> 
+        <input type="number" value={a} id="a" name="a" required onChange={(e) => setA(Number(e.target.value))}></input>
         <br></br>
-        <label for="b">b-vaule:</label>
-        <input type="number" id="b" name="b" required onChange={(e) => setB(Number(e.target.value))}></input>
+        <label /*for="b"*/>b-vaule:</label>
+        <input type="number" value={b} id="b" name="b" required onChange={(e) => setB(Number(e.target.value))}></input>
         <br></br>
-        <label for="c">c-vaule:</label>
-        <input type="number" id="c" name="c" required onChange={(e) => setC(Number(e.target.value))}></input>
+        <label /*for="c"*/>c-vaule:</label>
+        <input type="number" value={c} id="c" name="c" required onChange={(e) => setC(Number(e.target.value))}></input>
         <br></br>
-        <label for="d">d-vaule:</label> 
-        <input type="number" id="d" name="d" required onChange={(e) => setD(Number(e.target.value))}></input>
+        <label /*for="d"*/>d-vaule:</label> 
+        <input type="number" value={d} id="d" name="d" required onChange={(e) => setD(Number(e.target.value))}></input>
         <br></br>
         <input type="submit" value="Calculate" id="submit" 
-        
-        
-        >SAVE</input>
-        {/* send things to history */}
+        onClick={}
+        < History 
+        aVaule={a} 
+        bVaule={b} 
+        cVaule={c}
+        dVaule={d}/>
 
+        >SAVE</input>
+        {/* /* send things to history */ */}
+  
+  
+  
+     
       </form>
 
     </div>
   ); 
-};
-
-export const Vaule = () => {
-  
-
-  return(
-<>
-<table>
-<tr>
-<td>
-  <p>
-{a}
-</p>
-</td>
-</tr>
-
-</table>
-</>
-
-  );
 };
