afc4b03bbb0e954a48220f56974bc6e6397044d3
Keyao-Trinh <trinhkeyao@gmail.com>
2026-04-06 11:55:48 -0400

    tried to add tailwind

--- cube/src/Components/CubicInput.tsx
+++ cube/src/Components/CubicInput.tsx
@@ -3,6 +3,7 @@
 import { CubicSolver } from "./CubicEquation.tsx";
 import { History } from "./CubicHistory.tsx";
 
+
 import React, { useEffect, useState } from 'react';
 
 // import './index.css'
@@ -40,26 +41,27 @@ export const Input = (counting:number) => {
   
 
   return (
-    <div>
-      <h1>
+    <div class="relitive">
+      <h1 class="text-4xl font-serif p-2 static font-bold text-amber-700" >
         Cubic Solver
       </h1>
 
       <form name="theform" id="form" onSubmit={handleSubmit}>
 
-        <label /*for="a"*/>a-vaule:</label> 
+        <label /*for="a"*/ class="p-1 font-sans">a-vaule: </label> 
         <input type="number" value={a} id="a" name="a" required onChange={(e) => {setA(Number(e.target.value)); }}></input>
         <br></br>
-        <label /*for="b"*/>b-vaule:</label>
+        <label /*for="b"*/ class="p-1 font-sans"    >b-vaule: </label>
         <input type="number" value={b} id="b" name="b" required onChange={(e) => {setB(Number(e.target.value)); {handleClick};}}></input>
         <br></br>
-        <label /*for="c"*/>c-vaule:</label>
+        <label /*for="c"*/ class="p-1 font-sans" >c-vaule: </label>
         <input type="number" value={c} id="c" name="c" required onChange={(e) => {setC(Number(e.target.value)); {handleClick};}}></input>
         <br></br>
-        <label /*for="d"*/>d-vaule:</label> 
+        <label /*for="d"*/ class="p-1 font-sans" >d-vaule: </label> 
         <input type="number" value={d} id="d" name="d" required onChange={(e) => {setD(Number(e.target.value)); {handleClick};}}></input>
         <br></br>
         <button type="submit" value="Calculate" id="submit" 
+          class="outline-amber-700 rounded-lg p-2 border-2 border-grey-700 bg-amber-500 hover:bg-amber-700 font-sans text-amber-50 delay-100"
 onClick={handleClick}
       >SAVE</button>
       </form>
