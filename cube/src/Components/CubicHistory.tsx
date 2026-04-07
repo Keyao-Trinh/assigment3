dbcd3005510777c04444ecb169b28c1cb6392570
Keyao-Trinh <trinhkeyao@gmail.com>
2026-04-07 08:33:44 -0400

    edited something I hope it works

--- cube/src/Components/CubicHistory.tsx
+++ cube/src/Components/CubicHistory.tsx
@@ -1,9 +1,21 @@
 import {useRef, useState, useEffect} from "react";
+// import { CubicSolver } from "./CubicEquation.tsx";
+// import { History } from "./CubicHistory.tsx";
+// import { Table } from "./CubicTable.txs";
+// import { Graph } from "./CubicGraph.txs";
 import { Input } from "./CubicInput.tsx";
 
 
-
-export const History = (aVaule:number, bVaule:number, cVaule:number, dVaule:number, count:number) => {
+type abcdc = {aVaule:number, bVaule:number, cVaule:number, dVaule:number, count:number}
+export const History = ({aVaule, bVaule, cVaule, dVaule, count}:abcdc) => {
+    
+ let [counting, setCounting] = useState<number>(0);
+    
+  let a = Number(document.getElementById("a") as HTMLElement);
+  let b = Number(document.getElementById("b") as HTMLElement);
+  let c = Number(document.getElementById("c") as HTMLElement);
+  let d = Number(document.getElementById("d") as HTMLElement);
+    
     // set a,b,c,d to old vaules
     //   
     //
@@ -17,31 +29,29 @@ useEffect (() => {
   const bRef=useRef<number>(b);
   const cRef=useRef<number>(c);
   const dRef=useRef<number>(d);
-
+setCounting(counting+1);
 }, [count]);
   
-  let [a, setA] = useState<number>(0);
-  let [b, setB] = useState<number>(0);
-  let [c, setC] = useState<number>(0);
-  let [d, setD] = useState<number>(0);
-  
-  
-
-
+  // let [a, setA] = useState<number>(0);
+  // let [b, setB] = useState<number>(0);
+  // let [c, setC] = useState<number>(0);
+  // let [d, setD] = useState<number>(0);
   
 
 return(
     <div>
-
+<Input
+counting={counting}
+/>
 <table
    onClick={() => (Input.setA(Number(aRef)), Input.setB(Number(bRef)), Input.setC(Number(cRef)), Input.setD(Number(dRef)))}
   // document.getElementById("a") as HTMLElement 
   >
 <tr>
-<td>{aRef.current}</td>
-<td>{bRef.current}</td>
-<td>{cRef.current}</td>
-<td>{dRef.current}</td>
+<td id="ahistory" >{aRef.current}</td>
+<td id="bhistory" >{bRef.current}</td>
+<td id="chistory" >{cRef.current}</td>
+<td id="dhistory" >{dRef.current}</td>
 </tr>
 </table>
     </div>
